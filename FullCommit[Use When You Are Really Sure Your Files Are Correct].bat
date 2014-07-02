5def68148d4a15959ae112c94e5bd9d7b8765a9d
BackSpace47 <BackSpace47@outlook.com>
2014-07-01 19:51:36 -0500

    Full Commit MineFacility, Update Gratz Bro You Have Done a YoloSwaggins

---
+++ FullCommit[Use When You Are Really Sure Your Files Are Correct].bat
@@ -0,0 +1,30 @@
+@echo off
+color 12
+REM
+
+
+
+git status
+
+pause
+
+git add -A
+pause
+
+
+git commit -m "Full Commit MineFacility, Update Gratz Bro You Have Done a YoloSwaggins"
+pause
+
+
+git push
+pause
+
+
+
+echo Paradox Coders Team
+
+
+
+
+echo FINISHED
+pause

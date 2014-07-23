679aec425f03c3b19d8a9a83d106ef3f298836b5
BackSpace47 <BackSpace47@outlook.com>
2014-07-23 04:19:24 -0500

    Test Commit

--- GC.bat
+++ GC.bat
@@ -1,6 +1,73 @@
+
+@echo off
+title Git Commit
+color 12
+
+type NUL > Commit.txt
+
+
+
+
+set /p SomeVar=Enter Some Text Please
+
+
+
+
+
+
+
+echo %SomeVar%> Commit.txt
+
+dir Commit.txt
+
+
+
+git commit -m "%SomeVar%"
+
+pause
+
+type NUL > Commit.txt
+
+cls
+exit
+
+
+
+
+
+
+
+
+
+
+
+
+backspace notes
+
+
 @echo off
 color 12
+
+type NUL > Commit.txt
+
+
+
+
+echo|set /P ="hello" > Commit.txt
+
+dir Commit.txt
+
+
 
 git commit -m "MineFacility Update"
 
 pause
+
+type NUL > Commit.txt
+
+
+
+
+
+set /p SomeVar=What is your favourite colour? 
+echo Favourite colour: %SomeVar%

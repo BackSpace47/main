459ec89e6c173abb2d2e98dc8928ab6edc50b6d4
BackSpace47 <BackSpace47@outlook.com>
2014-07-23 04:24:20 -0500

    I need a

--- GC.bat
+++ GC.bat
@@ -1,5 +1,45 @@
 
-@echo off
+
+
+::===begin script
+@echo off & setlocal enabledelayedexpansion
+for /f "tokens=*" %%a in ('findstr /i /c:"<title>" *.htm') do (
+set x="%%a"
+:: you might get away with using a single delim char, but i tried to "safety" with 3.
+set x=!x:title^>=*@$!
+for /f "tokens=2 delims=*@$" %%b in (!x!) do (
+set y="%%b"
+set y=!y:~1,-3!
+echo Daily Quote: !y!
+>>titles echo !y!
+)
+)
+::=== end
+pause
+
+
+
+
+
+
+
+
+
+
+
+
+echo.
+echo.
+set i=10
+(Set /P j=Please Wait While We Process This...) < NUL
+ECHO.
+(Set /P j=......................................) < NUL
+:Start
+call :DisplayProgressBar %i%
+:replace the next line by the operation you want to do
+
+
+
 title Git Commit
 color 12
 
@@ -29,45 +69,30 @@ pause
 type NUL > Commit.txt
 
 cls
+
+
+echo Paradox Coders Team
+
+
+
+
+echo FINISHED
+pause
 exit
 
 
+set /a i = i + 1
+if /i %i% leq 100 goto start
+ECHO.
+ECHO.
+(Set /P j=Complete!) < NUL
+ECHO.
+PAUSE>NUL
+goto :EOF
 
+:DisplayProgressBar
+(Set /P j=.) < NUL
+title %1%% Completed
+exit /b
+endlocal
 
-
-
-
-
-
-
-
-
-backspace notes
-
-
-@echo off
-color 12
-
-type NUL > Commit.txt
-
-
-
-
-echo|set /P ="hello" > Commit.txt
-
-dir Commit.txt
-
-
-
-git commit -m "MineFacility Update"
-
-pause
-
-type NUL > Commit.txt
-
-
-
-
-
-set /p SomeVar=What is your favourite colour? 
-echo Favourite colour: %SomeVar%

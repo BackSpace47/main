2783e3c5a3e9413b926d8dbc8df6cdf633ba8822
Nicholas Boyd <njboyd@gmail.com>
2014-08-22 16:42:07 +0200

    no longer blank, but still not functional. Prepare yourself for the subjugation!

--- java/net/RPower/RPowermod/item/itemcyberWrench.java
+++ java/net/RPower/RPowermod/item/itemcyberWrench.java
@@ -1,7 +1,30 @@
 package net.RPower.RPowermod.item;
 
+import RPower.api.power.cable.I_MFCable;
+import net.RPower.RPowermod.machines.power.MFHelper;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 public class itemcyberWrench extends Item{
+
+	@Override
+	public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int metaD, float hitX, float hitY, float hitZ) {
+		if(world.getBlock(x, y, z).hasTileEntity(metaD))
+		{
+			TileEntity targetEntity = world.getTileEntity(x, y, z);
+
+			if(targetEntity instanceof I_MFCable)
+			{
+				System.out.println("it's an MF cable");
+			}
+
+
+
+		}
+		return super.onItemUse(itemStack, player, world, x, y, z, metaD, hitX, hitY, hitZ);
+	}
 
 }
